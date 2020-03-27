07b40dc4f382b6ef73bb0bac7aadcd6c74fde075
kpyuan1776 <Korbinian.Paul@googlemail.com>
2020-03-27 01:06:12 +0100

    sample component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlotlygraphComponent } from './plotlygraph/plotlygraph.component';
+
+
+import { PlotlyViaCDNModule  } from 'angular-plotly.js';
+PlotlyViaCDNModule.plotlyVersion = 'latest'; 
+PlotlyViaCDNModule.plotlyBundle = null;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlotlygraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlotlyViaCDNModule
   ],
   providers: [],
   bootstrap: [AppComponent]
